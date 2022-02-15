d4df8122db2b8d7e9d53e9ea8000b67dd48dc12a
Carlos Albeiro Galeano Gomez <99480165+Carlos0329@users.noreply.github.com>
2022-02-15 06:24:15 -0500

    Delete Ejercicio2.py

--- Ejercicio2.py
+++
@@ -1,9 +0,0 @@
-contador=1
-acumulado=0
-while contador<=10:
-    nota=float(input(f"Ingrese la nota {contador}: "))
-    acumulado=acumulado+nota
-    contador+=1
-total=acumulado/10
-print(f"El promedio es de: {total}")
-
